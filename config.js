1fe47405b37076e1ee6e7792b8271dd0a54fce51
coolfish <461836324@qq.com>
2015-04-13 23:06:21 +0800

    update

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	// mongodb 配置
+	MONGO_URI: "mongodb://127.0.0.1:27017/coolfish",
+	// 程序运行的端口
+	LISTEN_PORT: process.env.PORT || 9251,
+	// 网站名字
+	NAME: 'IT酷鱼网',
+	// 负责人
+	ADMIN: {
+		name: 'Yves',
+		qq: '461836324',
+		email: '461836324@qq.com'
+	}
+}
